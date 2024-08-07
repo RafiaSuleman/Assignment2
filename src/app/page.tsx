edd05638dc6c13a78bedb57fc879658b8fc4c9fe
RafiaSuleman <rafiasuleman47@gmail.com>
2024-08-07 10:27:23 +0500

    2nd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
     <nav className="bg-[#2563EB] text-white flex justify-center sm:justify-between  items-center flex-wrap px-0 sm:px-[90px] py-[16px]">
       <div><h1 className="text-white font-bold text-[25px]"> Assignment 2</h1></div>
         <div>
-          <ol className="list-none ml-[5px] flex gap-9 text-center flex-wrap justify-center item-center ">
+          <ol className="list-none ml-[5px] flex gap-3 text-center flex-wrap justify-center item-center ">
             <li>Home</li>
             <li>About</li>
             <li>Contact</li>
